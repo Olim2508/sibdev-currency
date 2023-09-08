211201bc21bc035dbbbbd2e00d393a5c8e783079
Olim2508 <rahmatovolim3@gmail.com>
2023-09-08 21:50:29 +0500

    fix scheme

--- web/auth_app/schemas.py
+++ web/auth_app/schemas.py
@@ -3,8 +3,8 @@ from drf_yasg import openapi
 
 logout_body_scheme = openapi.Schema(
     type=openapi.TYPE_OBJECT,
-    required=['key'],
+    required=['refresh'],
     properties={
-        'key': openapi.Schema(type=openapi.TYPE_STRING)
+        'refresh': openapi.Schema(type=openapi.TYPE_STRING)
     }
 )
